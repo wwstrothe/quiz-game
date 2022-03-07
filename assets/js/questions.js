34605ddc78e52edef74abc3654da914a8a3b94e7
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-03-06 19:29:40 -0700

    added questions to questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,22 @@
+var questionArray = [
+  {
+    question: "Select Option 1",
+    selection: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    answer: "Option 1",
+  },
+  {
+    question: "Select Option 2",
+    selection: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    answer: "Option 2",
+  },
+  {
+    question: "Select Option 3",
+    selection: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    answer: "Option 3",
+  },
+  {
+    question: "Select Option 4",
+    selection: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    answer: "Option 4",
+  }
+];
